667a02fa7c0ac4b5db07e9117452c42ef402d848
MD Omar Faruk <71517034+omarfaruk2020@users.noreply.github.com>
2020-09-22 10:07:10 +0600

    Java Script

---
+++ script.js
@@ -0,0 +1,43 @@
+var a,b,c;
+a=10;
+b=3;
+c=a+b;
+
+document.write("<br>");
+b++;
+
+
+
+
+
+
+  
+document.write("hello world");
+document.write("hello world");
+document.write("hello world");
+document.write("hello world");
+document.write("hello world");
+
+
+var jj =10;
+
+
+
+var kk=1;
+do {
+    document.write(kk + ") hello world <br>");
+    kk++;
+} while (kk <=10) ;
+    
+for(var kk=1 ; kk<=10; kk++ ) {
+    document.write(kk + ") Hello World <br>");
+}
+for(var kk=1 ; kk<=10; kk++ ) {
+    if(kk==3){
+    document.write(kk + ") Hey <br>");  
+
+    }
+    document.write(kk + ") Hello World <br>");
+    
+}
+
